2baa0c7b8025c24b8616af52d8caf1452d5c7271
niklasdewally <niklas@dewally.com>
2024-09-19 12:57:34 +0000

    Actions: Documentation for

--- docs/crates.js
+++ docs/crates.js
@@ -1,1 +1,2 @@
 window.ALL_CRATES = ["chuffed_rs","conjure_core","conjure_macros","conjure_oxide","enum_compatability_macro","minion_rs","uniplate"];
+//{"start":21,"fragment_lengths":[12,15,17,16,27,12,11]}
--- docs/trait.impl/core/iter/traits/collect/trait.IntoIterator.js
+++ docs/trait.impl/core/iter/traits/collect/trait.IntoIterator.js
@@ -1,3 +1,9 @@
-(function() {var implementors = {
-"uniplate":[["impl&lt;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sized.html\" title=\"trait core::marker::Sized\">Sized</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> + 'static&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/collect/trait.IntoIterator.html\" title=\"trait core::iter::traits::collect::IntoIterator\">IntoIterator</a> for <a class=\"enum\" href=\"uniplate/enum.Tree.html\" title=\"enum uniplate::Tree\">Tree</a>&lt;T&gt;"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {
+    var implementors = Object.fromEntries([["uniplate",[["impl&lt;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sized.html\" title=\"trait core::marker::Sized\">Sized</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> + 'static&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/collect/trait.IntoIterator.html\" title=\"trait core::iter::traits::collect::IntoIterator\">IntoIterator</a> for <a class=\"enum\" href=\"uniplate/enum.Tree.html\" title=\"enum uniplate::Tree\">Tree</a>&lt;T&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[730]}
