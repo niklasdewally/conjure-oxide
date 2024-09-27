ec000841b206b4907e70ad07c8fb9f935affee7e
niklasdewally <niklas@dewally.com>
2024-09-27 13:04:20 +0000

    Actions: Documentation for

--- docs/trait.impl/uniplate/biplate/trait.Uniplate.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["conjure_core",[["impl <a class=\"trait\" href=\"uniplate/biplate/trait.Uniplate.html\" title=\"trait uniplate::biplate::Uniplate\">Uniplate</a> for <a class=\"enum\" href=\"conjure_core/ast/enum.Expression.html\" title=\"enum conjure_core::ast::Expression\">Expression</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[278]}
