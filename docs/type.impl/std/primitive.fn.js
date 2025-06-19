b7cc2b97ba1ecf433f5a1ca6ffa5ca12a666f524
niklasdewally <niklas@dewally.com>
2025-06-19 14:42:10 +0000

    Actions: Rustdoc documentation for

--- docs/type.impl/std/primitive.fn.js
+++ docs/type.impl/std/primitive.fn.js
@@ -1,9 +1,9 @@
 (function() {
-    var type_impls = Object.fromEntries([["minion_rs",[]]]);
+    var type_impls = Object.fromEntries([["conjure_core",[]],["conjure_oxide",[]],["minion_rs",[]]]);
     if (window.register_type_impls) {
         window.register_type_impls(type_impls);
     } else {
         window.pending_type_impls = type_impls;
     }
 })()
-//{"start":55,"fragment_lengths":[16]}
+//{"start":55,"fragment_lengths":[19,21,17]}
